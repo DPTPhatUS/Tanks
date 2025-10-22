f9fac616d6e2cce073a80563241135fbc76c14c7
DPTPhatUS <tuanphat51sp@gmail.com>
2025-10-22 14:54:02 +0700

    local work

---
+++ Assets/_Tanks/Scripts/Tank/TankHealth.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Tanks.Complete
+{
+    public class TankHealth : MonoBehaviour
+    {
+        public float m_StartingHealth = 100f;
+        public Slider m_Slider;
+        public Image m_FillImage;
+        public Color m_FullHealthColor = Color.green;
+        public Color m_ZeroHealthColor = Color.red;
+        public GameObject m_ExplosionPrefab;
+        [HideInInspector] public bool m_HasShield;
+
+        private AudioSource m_ExplosionAudio;
+        private ParticleSystem m_ExplosionParticles;
+        private float m_CurrentHealth;
+        private bool m_Dead;
+        private float m_ShieldValue;
+        private bool m_IsInvincible;
+
+        private void Awake()
+        {
+            m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>();
+            m_ExplosionParticles.gameObject.SetActive(false);
+
+            m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>();
+
+            m_Slider.maxValue = m_StartingHealth;
+        }
+
+        private void OnDestroy()
+        {
+            if (m_ExplosionParticles != null)
+                Destroy(m_ExplosionParticles.gameObject);
+        }
+
+        private void OnEnable()
+        {
+            m_CurrentHealth = m_StartingHealth;
+            m_Dead = false;
+            m_HasShield = false;
+            m_ShieldValue = 0;
+            m_IsInvincible = false;
+
+            SetHealthUI();
+        }
+
+
+        public void TakeDamage(float amount)
+        {
+            if (!m_IsInvincible)
+            {
+                m_CurrentHealth -= amount * (1 - m_ShieldValue);
+
+                SetHealthUI();
+
+                if (m_CurrentHealth <= 0f && !m_Dead)
+                {
+                    OnDeath();
+                }
+            }
+        }
+
+
+        public void IncreaseHealth(float amount)
+        {
+            if (m_CurrentHealth + amount <= m_StartingHealth)
+            {
+                m_CurrentHealth += amount;
+            }
+            else
+            {
+                m_CurrentHealth = m_StartingHealth;
+            }
+
+            SetHealthUI();
+        }
+
+
+        public void ToggleShield(float shieldAmount)
+        {
+            m_HasShield = !m_HasShield;
+
+            if (m_HasShield)
+            {
+                m_ShieldValue = shieldAmount;
+            }
+            else
+            {
+                m_ShieldValue = 0;
+            }
+        }
+
+        public void ToggleInvincibility()
+        {
+            m_IsInvincible = !m_IsInvincible;
+        }
+
+
+        private void SetHealthUI()
+        {
+            m_Slider.value = m_CurrentHealth;
+            m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
+        }
+
+
+        private void OnDeath()
+        {
+            m_Dead = true;
+
+            m_ExplosionParticles.transform.position = transform.position;
+            m_ExplosionParticles.gameObject.SetActive(true);
+            m_ExplosionParticles.Play();
+
+            m_ExplosionAudio.Play();
+
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/_Tanks/Scripts/Tank/TankShooting.cs
@@ -0,0 +1,229 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+namespace Tanks.Complete
+{
+    public class TankShooting : MonoBehaviour
+    {
+        public Rigidbody m_Shell;
+        public Transform m_FireTransform;
+        public Slider m_AimSlider;
+        public AudioSource m_ShootingAudio;
+        public AudioClip m_ChargingClip;
+        public AudioClip m_FireClip;
+        public float m_MinLaunchForce = 5f;
+        public float m_MaxLaunchForce = 20f;
+        public float m_MaxChargeTime = 0.75f;
+        public float m_ShotCooldown = 1.0f;
+        public float m_MaxDamage = 100f;
+        public float m_ExplosionForce = 50f;
+        public float m_ExplosionRadius = 5f;
+
+        [HideInInspector]
+        public TankInputUser m_InputUser;
+
+        public float CurrentChargeRatio => (m_CurrentLaunchForce - m_MinLaunchForce) / (m_MaxLaunchForce - m_MinLaunchForce);
+        public bool IsCharging => m_IsCharging;
+
+        public bool m_IsComputerControlled { get; set; } = false;
+
+        private string m_FireButton;
+        private float m_CurrentLaunchForce;
+        private float m_ChargeSpeed;
+        private bool m_Fired;
+        private bool m_HasSpecialShell;
+        private float m_SpecialShellMultiplier;
+        private InputAction fireAction;
+        private bool m_IsCharging = false;
+        private float m_BaseMinLaunchForce;
+        private float m_ShotCooldownTimer;
+
+        private void OnEnable()
+        {
+            m_CurrentLaunchForce = m_MinLaunchForce;
+            m_BaseMinLaunchForce = m_MinLaunchForce;
+            m_AimSlider.value = m_BaseMinLaunchForce;
+            m_HasSpecialShell = false;
+            m_SpecialShellMultiplier = 1.0f;
+
+            m_AimSlider.minValue = m_MinLaunchForce;
+            m_AimSlider.maxValue = m_MaxLaunchForce;
+        }
+
+        private void Awake()
+        {
+            m_InputUser = GetComponent<TankInputUser>();
+            if (m_InputUser == null)
+                m_InputUser = gameObject.AddComponent<TankInputUser>();
+        }
+
+        private void Start()
+        {
+            m_FireButton = "Fire";
+            fireAction = m_InputUser.ActionAsset.FindAction(m_FireButton);
+
+            fireAction.Enable();
+
+            m_ChargeSpeed = (m_MaxLaunchForce - m_MinLaunchForce) / m_MaxChargeTime;
+        }
+
+
+        private void Update()
+        {
+            if (!m_IsComputerControlled)
+            {
+                HumanUpdate();
+            }
+            else
+            {
+                ComputerUpdate();
+            }
+        }
+
+        public void StartCharging()
+        {
+            m_IsCharging = true;
+            m_Fired = false;
+            m_CurrentLaunchForce = m_MinLaunchForce;
+
+            m_ShootingAudio.clip = m_ChargingClip;
+            m_ShootingAudio.Play();
+        }
+
+        public void StopCharging()
+        {
+            if (m_IsCharging)
+            {
+                Fire();
+                m_IsCharging = false;
+            }
+        }
+
+        void ComputerUpdate()
+        {
+            m_AimSlider.value = m_BaseMinLaunchForce;
+
+            if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+            {
+                m_CurrentLaunchForce = m_MaxLaunchForce;
+                Fire();
+            }
+            else if (m_IsCharging && !m_Fired)
+            {
+                m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+
+                m_AimSlider.value = m_CurrentLaunchForce;
+            }
+            else if (fireAction.WasReleasedThisFrame() && !m_Fired)
+            {
+                Fire();
+                m_IsCharging = false;
+            }
+        }
+
+        void HumanUpdate()
+        {
+            if (m_ShotCooldownTimer > 0.0f)
+            {
+                m_ShotCooldownTimer -= Time.deltaTime;
+            }
+
+            m_AimSlider.value = m_BaseMinLaunchForce;
+
+            if (m_CurrentLaunchForce >= m_MaxLaunchForce && !m_Fired)
+            {
+                m_CurrentLaunchForce = m_MaxLaunchForce;
+                Fire();
+            }
+            else if (m_ShotCooldownTimer <= 0 && fireAction.WasPressedThisFrame())
+            {
+                m_Fired = false;
+                m_CurrentLaunchForce = m_MinLaunchForce;
+
+                m_ShootingAudio.clip = m_ChargingClip;
+                m_ShootingAudio.Play();
+            }
+            else if (fireAction.IsPressed() && !m_Fired)
+            {
+                m_CurrentLaunchForce += m_ChargeSpeed * Time.deltaTime;
+
+                m_AimSlider.value = m_CurrentLaunchForce;
+            }
+            else if (fireAction.WasReleasedThisFrame() && !m_Fired)
+            {
+                Fire();
+            }
+        }
+
+
+        private void Fire()
+        {
+            m_Fired = true;
+
+            Rigidbody shellInstance = Instantiate(m_Shell, m_FireTransform.position, m_FireTransform.rotation) as Rigidbody;
+
+            shellInstance.linearVelocity = m_CurrentLaunchForce * m_FireTransform.forward;
+
+            ShellExplosion explosionData = shellInstance.GetComponent<ShellExplosion>();
+            explosionData.m_ExplosionForce = m_ExplosionForce;
+            explosionData.m_ExplosionRadius = m_ExplosionRadius;
+            explosionData.m_MaxDamage = m_MaxDamage;
+
+            if (m_HasSpecialShell)
+            {
+                explosionData.m_MaxDamage *= m_SpecialShellMultiplier;
+                m_HasSpecialShell = false;
+                m_SpecialShellMultiplier = 1f;
+
+                PowerUpDetector powerUpDetector = GetComponent<PowerUpDetector>();
+                if (powerUpDetector != null)
+                    powerUpDetector.m_HasActivePowerUp = false;
+
+                PowerUpHUD powerUpHUD = GetComponentInChildren<PowerUpHUD>();
+                if (powerUpHUD != null)
+                    powerUpHUD.DisableActiveHUD();
+            }
+
+            m_ShootingAudio.clip = m_FireClip;
+            m_ShootingAudio.Play();
+
+            m_CurrentLaunchForce = m_MinLaunchForce;
+
+            m_ShotCooldownTimer = m_ShotCooldown;
+        }
+
+
+        public void EquipSpecialShell(float damageMultiplier)
+        {
+            m_HasSpecialShell = true;
+            m_SpecialShellMultiplier = damageMultiplier;
+        }
+
+        public Vector3 GetProjectilePosition(float chargingLevel)
+        {
+            float chargeLevel = Mathf.Lerp(m_MinLaunchForce, m_MaxLaunchForce, chargingLevel);
+            Vector3 velocity = chargeLevel * m_FireTransform.forward;
+
+            float a = 0.5f * Physics.gravity.y;
+            float b = velocity.y;
+            float c = m_FireTransform.position.y;
+
+            float sqrtContent = b * b - 4 * a * c;
+            if (sqrtContent <= 0)
+            {
+                return m_FireTransform.position;
+            }
+
+            float answer1 = (-b + Mathf.Sqrt(sqrtContent)) / (2 * a);
+            float answer2 = (-b - Mathf.Sqrt(sqrtContent)) / (2 * a);
+
+            float answer = answer1 > 0 ? answer1 : answer2;
+
+            Vector3 position = m_FireTransform.position + new Vector3(velocity.x, 0, velocity.z) * answer;
+            position.y = 0;
+
+            return position;
+        }
+    }
+}
